b433214a55d9d1e3e209584d863d37530dfa42c6
WojtusMientus <WojtusMientus@users.noreply.github.com>
2025-09-01 15:22:16 +0200

    Initial commit

---
+++ Source/Boids_Project/Boids_Project.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "Boids_Project.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Boids_Project, "Boids_Project" );
